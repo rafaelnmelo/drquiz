7cc4f16d2617bbad6035b7183c483d2e456a8a35
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-07-19 10:35:23 -0300

    UI Test ao selecionar quiz

--- DrQuiz/DrQuizUITests/DrQuizUITests.swift
+++ DrQuiz/DrQuizUITests/DrQuizUITests.swift
@@ -3,7 +3,6 @@ import XCTest
 class when_app_is_launched: XCTestCase {
     
     func test_should_display_available_quizzes() {
-        
         let app = XCUIApplication()
         app.launchEnvironment = ["ENV": "TEST"]
         continueAfterFailure = false
@@ -15,4 +14,21 @@ class when_app_is_launched: XCTestCase {
         XCTAssertEqual(2, quizList.cells.count)
     }
    
+}
+
+class when_user_taps_on_a_quiz: XCTestCase {
+    
+    func test_should_display_questions_for_the_selected_quiz() {
+        let app = XCUIApplication()
+        app.launchEnvironment = ["ENV": "TEST"]
+        continueAfterFailure = false
+        app.launch()
+        
+        let quizList = app.tables["quizList"]
+        quizList.cells["Math 101"].tap()
+        
+        let _ = app.tables["questionList"].waitForExistence(timeout: 2.0)
+        XCTAssertEqual(3, app.tables["questionList"].cells.count)
+    }
+    
 }
